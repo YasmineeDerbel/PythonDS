4533189ebed49025d547a8db4c59b8982e71136c
Yasmine Derbel <yasminederbel002@gmail.com>
2024-02-01 14:43:36 +0100

    exemple1: variables,types de bases, print

---
+++ seance1/example1.py
@@ -0,0 +1,10 @@
+#en mode script on code dans .py
+
+nom="ali" #variable string
+age=30 #variable int
+salaire=1200.500 #variable float
+celibataire = True #variable bool
+#afficher les informations avec print 
+print("hello",end="|")
+print (nom,age,celibataire,sep=";")
+print("l'employé",nom,"est agé de",age,"ans")
